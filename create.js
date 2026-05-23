5a757a4429c0743079b05cef9085968ca72ad47c
earnerx3 <earnerx3@gmail.com>
2026-05-23 11:45:40 +0600

    Create create.js

---
+++ create.js
@@ -0,0 +1,18 @@
+export default async function handler(req, res) {
+    if (req.method !== 'POST') return res.status(405).json({ error: 'Only POST request allowed' });
+
+    try {
+        const response = await fetch('https://pay.bdusp.com/api/payment/create', {
+            method: 'POST',
+            headers: { 
+                'Content-Type': 'application/json' 
+            },
+            body: JSON.stringify(req.body) 
+        });
+        
+        const data = await response.json();
+        res.status(200).json(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Internal Server Error' });
+    }
+}
