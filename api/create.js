4514ebc8305ee4c4cae85acbfea5f2b9e6d7b369
earnerx3 <earnerx3@gmail.com>
2026-05-23 12:57:35 +0600

    Update create.js

--- api/create.js
+++ api/create.js
@@ -2,13 +2,16 @@ export default async function handler(req, res) {
     if (req.method !== 'POST') return res.status(405).json({ error: 'Only POST allowed' });
 
     try {
+        const payload = req.body;
+        // BDUSP এর নিয়ম অনুযায়ী api_key টা বডির ভেতরেই পাঠানো বেশি নিরাপদ
+        payload.api_key = '7ScZpy6qgmrfgG9JbQb8yOqSj2PhQPCKX8q2Lbfb8ZQYrMcHCN';
+
         const response = await fetch('https://pay.bdusp.com/api/payment/create', {
             method: 'POST',
             headers: { 
-                'Content-Type': 'application/json',
-                'Authorization': 'Bearer 7ScZpy6qgmrfgG9JbQb8yOqSj2PhQPCKX8q2Lbfb8ZQYrMcHCN' // আপনার Key টি এখানে দিলাম
+                'Content-Type': 'application/json' 
             },
-            body: JSON.stringify(req.body) 
+            body: JSON.stringify(payload) 
         });
         
         const data = await response.json();
